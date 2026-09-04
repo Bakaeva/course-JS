ca17db0530cd3de259267f6374c4a0aff5e5f73d
Bakaeva <l_bakaeva74@mail.ru>
2026-09-04 14:19:54 +0300

    Выполнено дз урока по Git

---
+++ script.js
@@ -0,0 +1,10 @@
+let title = "";
+let screens = "";
+let screenPrice = 0;
+let rollback = 0;
+let fullPrice = 0;
+let adaptive = true;
+
+alert("alert-сообщение");
+
+console.log("console-сообщение");
