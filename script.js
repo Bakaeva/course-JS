1565ea28491b0f43d342cd7d87be2a162c8ed2c7
Bakaeva <l_bakaeva74@mail.ru>
2026-09-04 15:51:01 +0300

    Выполнено дз урока по типам данных и операторам

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let title = "";
-let screens = "";
-let screenPrice = 0;
-let rollback = 0;
-let fullPrice = 0;
+let title = "Название проекта";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 5000;
+let rollback = 20;
+let fullPrice = 100000;
 let adaptive = true;
 
-alert("alert-сообщение");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log("console-сообщение");
+console.log(screens.length);
+
+console.log(
+  "Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани",
+);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани",
+);
+console.log(screens.toLowerCase().split(", "));
+console.log(
+  "Процент отката посреднику за работу " + fullPrice * (rollback / 100),
+);
